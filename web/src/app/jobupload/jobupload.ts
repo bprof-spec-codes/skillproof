ad97577219d91a454b498cfaa1c7503cd609d74f
Benjamin <kvbenjamin@stud.uni-obuda.hu>
2026-03-15 14:36:29 +0100

    start working

---
+++ web/src/app/jobupload/jobupload.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jobupload',
+  standalone: false,
+  templateUrl: './jobupload.html',
+  styleUrl: './jobupload.scss',
+})
+export class Jobupload {}
