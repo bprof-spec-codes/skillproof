048cfb8e3be1956f4d003caa418d5ee32658f6dd
KelemenOzseb <kelemenozzseb@gmail.com>
2026-03-17 02:27:04 +0100

    add repository

---
+++ backend/SkillProof/SkillProof.Data/Repositorys/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using SkillProof.Entities.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillProof.Data.Repositorys
+{
+    public interface IRepository<T> where T : class, IIdentity
+    {
+        Task<T> Create(T entity);
+        Task DeleteById(string id);
+        Task<T?> GetOne(string id);
+        IQueryable<T> GetAll();
+        Task<T> Update(T entity);
+    }
+}
