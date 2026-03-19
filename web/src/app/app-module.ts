3b30a4d31e4ebdc4e85a61bff9af8976ffbd92d3
zadoriaron <789zadori.aron@gmail.com>
2026-03-19 22:11:45 +0100

    Program.cs and some other nupackage fixed and setted

--- web/src/app/app-module.ts
+++ web/src/app/app-module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Login, Register],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
--- web/src/app/app-routing-module.ts
+++ web/src/app/app-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"login", pathMatch:"full"},
+  {path:"login", component:Login},
+  {path:"register", component:Register},
+  {path:"**", redirectTo:"login", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
