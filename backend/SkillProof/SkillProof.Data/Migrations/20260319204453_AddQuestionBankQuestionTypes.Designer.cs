e2bb0d10e7ce7c5ddeab7c2ae511d9b10dd9c982
oli-tolnai <oli.tolnai3@gmail.com>
2026-03-19 21:47:21 +0100

    Add EF migration for Question Bank subtype schema

--- backend/SkillProof/SkillProof.Data/Migrations/20260319204453_AddQuestionBankQuestionTypes.Designer.cs
+++ backend/SkillProof/SkillProof.Data/Migrations/20260319204453_AddQuestionBankQuestionTypes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SkillProof.Data;
 
@@ -11,9 +12,11 @@ using SkillProof.Data;
 namespace SkillProof.Data.Migrations
 {
     [DbContext(typeof(SkillProofDbContext))]
-    partial class SkillProofDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260319204453_AddQuestionBankQuestionTypes")]
+    partial class AddQuestionBankQuestionTypes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/SkillProof/SkillProof.Data/Migrations/20260319204453_AddQuestionBankQuestionTypes.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillProof.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuestionBankQuestionTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TrueFalseQuestions",
+                columns: table => new
+                {
+                    QuestionId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CorrectAnswer = table.Column<bool>(type: "bit", nullable: false),
+                    Explanation = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrueFalseQuestions", x => x.QuestionId);
+                    table.ForeignKey(
+                        name: "FK_TrueFalseQuestions_Questions_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TrueFalseQuestions");
+        }
+    }
+}
