d8e58ef7da6f402ab485e75a7af50a0e3d5c2930
zadoriaron <789zadori.aron@gmail.com>
2026-03-19 22:11:45 +0100

    Program.cs and some other nupackage fixed and setted

--- backend/SkillProof/SkillProof.Api/Program.cs
+++ backend/SkillProof/SkillProof.Api/Program.cs
@@ -1,35 +1,148 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SkillProof.Data;
+using SkillProof.Entities.Helper;
 using SkillProof.Entities.Models;
+using System.Text;
 
 namespace SkillProof.Api
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            //Connection string from json file
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            //JWT configuration from json file
+            //The defined properties are now in a class, inside the properties of it
+            builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
+
+            var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>();
+
+            var jwtIssuer = jwtSettings!.Issuer;
+            var jwtKey = jwtSettings!.Key;
+
+            //Cors configuration from json file
+            var frontendUrl = builder.Configuration["settings:frontend"];        
+
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddCors(option =>
+            {
+                option.AddPolicy("AllowAngularApp", policy =>
+                {
+                    policy.WithOrigins(frontendUrl!)
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                });
+            });
+
+            //Auth
+
+            builder.Services.AddIdentity<Users, IdentityRole>(
+                option =>
+                {
+                    option.Password.RequireDigit = false;
+                    option.Password.RequiredLength = 8;
+                    option.Password.RequireNonAlphanumeric = false;
+                    option.Password.RequireUppercase = false;
+                    option.Password.RequireLowercase = false;
+                })
+                .AddEntityFrameworkStores<SkillProofDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = jwtIssuer,
+                    ValidIssuer = jwtIssuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+                };
+            }); ;
+
+            //DbCtx
 
             builder.Services.AddDbContext<SkillProofDbContext>(options =>
                 options.UseSqlServer(builder.Configuration["db:conn"]));
 
-            builder.Services.AddIdentity<Users, IdentityRole>()
-                .AddEntityFrameworkStores<SkillProofDbContext>()
-                .AddDefaultTokenProviders();
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+            builder.Services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Edushare API", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+  
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("AllowAngularApp");
+
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
+
+            app.UseStaticFiles();
 
             app.MapControllers();
 
