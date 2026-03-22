c9a5c0be4c7608f2d322ed915b6e4f7cf5e0d7f7
KelemenOzseb <kelemenozzseb@gmail.com>
2026-03-22 22:53:56 +0100

    error interceptor added

--- web/src/app/app-module.ts
+++ web/src/app/app-module.ts
@@ -6,14 +6,19 @@ import { App } from './app';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { Header } from './header/header';
 import { HomePage } from './home-page/home-page';
+import { errorInterceptor } from './interceptors/error-interceptor';
 
 @NgModule({
   declarations: [App, Login, Register, Header, HomePage],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule,FormsModule],
-  providers: [provideBrowserGlobalErrorListeners()],
+  providers: [provideBrowserGlobalErrorListeners(),
+    provideHttpClient(
+      withInterceptors([errorInterceptor])
+    )
+  ],
   bootstrap: [App],
 })
 export class AppModule {}
---
+++ web/src/app/interceptors/error-interceptor.ts
@@ -0,0 +1,43 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from '../../environments/environment.development';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router)
+  return next(req).pipe(
+    catchError((err: HttpErrorResponse) => {
+      let userMessage = "Ismeretlen hiba történt."
+
+      if (err.status === 0) {
+        userMessage = "Hálózati hiba: a szerver nem elérhető (CORS vagy offline)."
+      }
+      else if (err.error?.message || err.error?.error) {
+        userMessage = err.error.message ?? err.error.error
+      } else if (err.status === 401) {
+        userMessage = "A munkamenet lejárt vagy érvénytelen. Jelentkezz be újra."
+        localStorage.removeItem('skillProof_token')
+        router.navigate(['/login'])
+      } else if (err.status === 403) {
+        userMessage = "Nincs jogosultságod a művelethez."
+      } else if (err.status === 404) {
+        userMessage = "Az erőforrás nem található."
+      } else if (err.status >= 500) {
+        userMessage = "Szerverhiba történt. Próbáld meg később."
+      }
+      
+      // modalt ide
+
+      console.error("[HTTP ERROR]", {
+        method: req.method,
+        url: req.urlWithParams,
+        status: err.status,
+        error: err
+      });
+
+      (err as any).userMessage = userMessage
+      return throwError(() => err) 
+    }),
+  )
+};
