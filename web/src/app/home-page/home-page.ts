d8cc755c6707b7d94ad3b100c73dc7c80911b5a9
KelemenOzseb <kelemenozzseb@gmail.com>
2026-03-22 22:28:41 +0100

    job search function done

--- web/src/app/home-page/home-page.ts
+++ web/src/app/home-page/home-page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { JobService } from '../services/job-service';
+import { JobViewDto } from '../Models/Dtos/Job/JobView-dto';
+import levenshtein from 'fast-levenshtein'; // a hibapontos keresést egyszerűsití meg
+import { BehaviorSubject, combineLatest, debounceTime, map, Observable, of, startWith, take } from 'rxjs';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
@@ -6,4 +11,70 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
-export class HomePage {}
+export class HomePage {
+  jobs: JobViewDto[] = []
+  constructor(public jobService: JobService){
+  }
+
+  freeTextControl = new FormControl('')
+  locationControl = new FormControl('')
+  filteredJobs = new BehaviorSubject<JobViewDto[]>([])
+  filteredJobs$: Observable<JobViewDto[]> = of([])
+
+  ngOnInit() {
+    
+  }
+  
+  search() {
+  this.jobService.jobs$.pipe(take(1)).subscribe(allJobs => {
+    
+    const fText = this.freeTextControl.value?.toLowerCase().trim() || ''
+    const lText = this.locationControl.value?.toLowerCase().trim() || ''
+
+    // Ha nincs keresési feltétel, visszaadjuk az összeset
+    if (!fText && !lText) {
+      this.filteredJobs.next(allJobs)
+      return
+    }
+
+    let threshold = 0
+    const fLength = fText.length
+    if (fLength > 3 && fLength < 10) threshold = 1
+    else if (fLength >= 10 && fLength < 16) threshold = 2
+    else if (fLength >= 16) threshold = 3
+
+    const results = allJobs
+      .map(job => {
+        let score = 0
+
+        if (lText && job.location) {
+          const dist = levenshtein.get(lText, job.location.toLowerCase())
+          if (dist <= threshold) {
+            score += (threshold + 1 - dist) * 5
+          }
+        }
+
+        if (fText) {
+          const titleDist = levenshtein.get(fText, job.title.toLowerCase())
+          if (titleDist <= threshold) {
+            score += (threshold + 1 - titleDist) * 3
+          }
+
+          job.tags?.forEach(tag => {
+            const tagDist = levenshtein.get(fText, tag.toLowerCase())
+            if (tagDist <= threshold) {
+              score += (threshold + 1 - tagDist)
+            }
+          });
+        }
+
+        return { job, score }
+      })
+      .filter(item => item.score >= 2)
+      .sort((a, b) => b.score - a.score)
+      .map(item => item.job)
+
+    this.filteredJobs.next(results)
+  })
+  }
+}
