ba6469f24fb899e0001be6c9158175351c5d7a3a
KelemenOzseb <kelemenozzseb@gmail.com>
2026-03-22 22:43:06 +0100

    exception middleware added

---
+++ backend/SkillProof/SkillProof.Api/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Authentication;
+
+namespace SkillProof.Api
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            // Meghatározza a Status Code-ot a hiba típusa alapján, aki úgy érzi még további hibákat is hozzáadhat
+            context.Response.StatusCode = exception switch
+            {
+                ArgumentException => StatusCodes.Status400BadRequest,
+                AuthenticationException => StatusCodes.Status401Unauthorized,
+                UnauthorizedAccessException => StatusCodes.Status403Forbidden,
+                KeyNotFoundException => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            var response = new
+            {
+                message = exception.Message,
+                type = exception.GetType().Name
+            };
+
+            return context.Response.WriteAsJsonAsync(response);
+        }
+    }
+}
