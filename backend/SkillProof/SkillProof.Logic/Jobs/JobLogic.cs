20284ec286bf3d498368db68dbc002def55172d8
Kanya Marcell <marcikanya@gmail.com>
2026-03-22 10:54:27 +0100

    jobs crud logic

--- backend/SkillProof/SkillProof.Logic/Jobs/JobLogic.cs
+++ backend/SkillProof/SkillProof.Logic/Jobs/JobLogic.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using SkillProof.Data.Repositorys;
 using SkillProof.Entities.Dtos.Job;
 using SkillProof.Entities.Models;
@@ -30,5 +31,53 @@ public class JobLogic : IJobLogic
         await _jobRepository.Create(newJob);
 
         return newJob;
+    }
+    
+    public async Task<IEnumerable<Job>> GetAllJobsAsync()
+    {
+        return await _jobRepository.GetAll().ToListAsync();
+    }
+
+    public async Task<Job?> GetJobByIdAsync(string id)
+    {
+        return await _jobRepository.GetOne(id);
+    }
+
+    public async Task<Job> UpdateJobAsync(string id, JobCreateDto model, string companyId)
+    {
+        var job = await _jobRepository.GetOne(id);
+        if (job == null)
+        {
+            throw new Exception("The job is not found.");
+        }
+
+        if (job.CompanyId != companyId)
+        {
+            throw new UnauthorizedAccessException("You do not have permission to modify this job.\n");
+        }
+
+        job.Title = model.Title;
+        job.Description = model.Description;
+        job.Location = model.Location;
+        job.Tags = model.Tags;
+        job.EmploymentType = model.EmploymentType;
+
+        return await _jobRepository.Update(job);
+    }
+
+    public async Task DeleteJobAsync(string id, string companyId)
+    {
+        var job = await _jobRepository.GetOne(id);
+        if (job == null)
+        {
+            throw new Exception("The job is not found.");
+        }
+
+        if (job.CompanyId != companyId)
+        {
+            throw new UnauthorizedAccessException("You do not have permission to modify this job.\n");
+        }
+
+        await _jobRepository.DeleteById(id);
     }
 }
