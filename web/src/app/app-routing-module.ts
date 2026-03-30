e6427ea225792a2eb701ee347dc90a5b7411c4d3
zadoriaron <789zadori.aron@gmail.com>
2026-03-30 22:59:02 +0200

    commit, conflict modify

--- web/src/app/app-routing-module.ts
+++ web/src/app/app-routing-module.ts
@@ -6,16 +6,20 @@ import { HomePage } from './home-page/home-page';
 import { QuestionBankList } from './components/question-bank-list/question-bank-list';
 import { QuestionBankForm } from './components/question-bank-form/question-bank-form';
 import { QuestionBankDetails } from './components/question-bank-details/question-bank-details';
+import { EditProfile } from './components/edit-profile/edit-profile';
+import { ProfileView } from './components/profile-view/profile-view';
 
 const routes: Routes = [
   {path:"", redirectTo:"home", pathMatch:"full"},
-  {path:"home", component: HomePage},
+  {path:"homePage", component: HomePage},
   {path:"login", component:Login},
   {path:"register", component:Register},
   {path:"question-bank", component:QuestionBankList},
   {path:"question-bank/create", component:QuestionBankForm},
   {path:"question-bank/:id/edit", component:QuestionBankForm},
   {path:"question-bank/:id", component:QuestionBankDetails},
+  {path:"editProfile", component:EditProfile},
+  {path:"viewProfile", component:ProfileView},
   {path:"**", redirectTo:"home", pathMatch:"full"}
 ];
 
