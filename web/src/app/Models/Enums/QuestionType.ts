1aa9ab210dc736dc249e8e6df75dc201829a19e4
oli-tolnai <tolnaioli@gmail.com>
2026-04-23 00:53:33 +0200

    change QuestionType enum to text

--- web/src/app/Models/Enums/QuestionType.ts
+++ web/src/app/Models/Enums/QuestionType.ts
@@ -1,6 +1,6 @@
 export enum QuestionType {
-  MultipleChoice = 0,
-  CodeCompletion = 1,
-  TrueFalse = 2,
-  FillInTheBlank = 3,
+  MultipleChoice = 'MultipleChoice',
+  CodeCompletion = 'CodeCompletion',
+  TrueFalse = 'TrueFalse',
+  FillInTheBlank = 'FillInTheBlank',
 }
