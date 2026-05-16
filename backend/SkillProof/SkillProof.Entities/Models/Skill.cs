5dd7d685038751d1b2272afe5fd9ef090fd8e5cd
zadoriaron <789zadori.aron@gmail.com>
2026-05-16 12:31:56 +0200

    Skill entity added

---
+++ backend/SkillProof/SkillProof.Entities/Models/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillProof.Entities.Models
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Assessments> Assesments { get; set; }
+    }
+}
